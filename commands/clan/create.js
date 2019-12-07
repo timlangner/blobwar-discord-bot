dd18948c3272315482eeaada3d567831068f1a9f
Tim Langner <tim.langner0@gmail.com>
2019-12-07 16:22:28 +0100

    :sparkles: Add command to create a "Clan" as a clan leader

--- commands/clan/create.js
+++ commands/clan/create.js
@@ -5,16 +5,19 @@ module.exports = {
     name: 'create',
     description: 'Creates a new clan!',
     guildOnly: true,
-    execute(message, args) {
+    async execute(message, args) {
         if (!args.length) {
             return message.channel.send(`Unknown command. Use ${prefix}c help to get a list of all commands.`)
         } else if (args[0] === 'create') {
             if (args.length >= 1 && args.length < 2) {
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
             }
-            // message.guild.createRole({ name: args[1], permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] });
-            // const createdRole = message.guild.roles.find(role => role.name === args[1]);
-            // message.member.addRole(createdRole);
+            //Creates a new role with the name of the second argument
+            const createdRole = await message.guild.createRole({
+                name: args[1],
+                permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS']
+            });
+            message.member.addRole(createdRole).then(r => console.log(createdRole));
             return message.channel.send(`You've successfully created the clan **${args[1]}**`);
         }
     },
