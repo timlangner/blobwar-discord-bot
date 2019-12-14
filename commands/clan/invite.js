91e54e90c63252ea906cdf97c3b84e7a29f8a563
Tim Langner <tim.langner0@gmail.com>
2019-12-14 15:28:47 +0100

    :sparkles: Add !invite command with embeds

---
+++ commands/clan/invite.js
@@ -0,0 +1,62 @@
+
+const Discord = require('discord.js');
+const {prefix} = require('../../config');
+
+module.exports = {
+    name: 'invite',
+    description: 'Invites a user to a Clan',
+    guildOnly: true,
+    async execute(message, args) {
+        if (!args.length) {
+            return message.channel.send(`Unknown command. Use ${prefix}help to get a list of all commands.`)
+        } else if (args.length >= 0 && args.length < 0) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+
+        const mentionedUser = message.mentions.users.first();
+        const ownerUserId = '185053226641522690';
+        const authorUserId = message.author.id;
+        const authorUsername = (await message.client.fetchUser(message.author.id)).username;
+        const ownerAvatar = (await message.client.fetchUser(ownerUserId)).avatarURL;
+        const authorAvatar = (await message.client.fetchUser(authorUserId)).avatarURL;
+
+        const inviteEmbed = new Discord.RichEmbed()
+            .setColor('#0000ff')
+            .setTitle('You got invited!')
+            .setDescription(`${authorUsername} invited you to join the Clan **CouchPotato**! React to accept or decline.`)
+            .setThumbnail(authorAvatar);
+
+        const acceptEmbed = new Discord.RichEmbed()
+            .setColor('#40ff00')
+            .setTitle('SUCCESS')
+            .setDescription(`You've successfully accepted the invitation and received access to your clan area.`);
+
+        const declineEmbed = new Discord.RichEmbed()
+            .setColor('#ff0000')
+            .setTitle('SUCCESS')
+            .setDescription(`You've successfully declined the invitation and will not receive access to the clan area.`);
+
+        mentionedUser.send(inviteEmbed).then(embedMessage => {
+            embedMessage.react('✅').then(() => embedMessage.react('❌'));
+
+            const filter = (reaction, user) => {
+                return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
+            };
+
+            embedMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+                .then(collected => {
+                    const reaction = collected.first();
+
+                    if (reaction.emoji.name === '✅') {
+                        // give role to reacted user
+                        mentionedUser.send(acceptEmbed);
+                    } else {
+                       mentionedUser.send(declineEmbed);
+                    }
+                })
+                .catch(collected => {
+                    message.channel.send('you reacted with neither a thumbs up, nor a thumbs down.');
+                });
+        });
+    }
+};
