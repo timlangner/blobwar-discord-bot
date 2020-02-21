1bd4fb1e04d3563920da6fa3c1ceb168490e7b14
Tim <tim.langner@tobit.software>
2020-02-21 15:04:15 +0100

    :fire: Remove unused function

--- commands/clan/profile.js
+++ commands/clan/profile.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 
-// const sec2time = (timeInSeconds) => {
-//     let pad = function(num, size) { return ('000' + num).slice(size * -1); },
-//         time = parseFloat(timeInSeconds).toFixed(3),
-//         hours = Math.floor(time / 60 / 60),
-//         minutes = Math.floor(time / 60) % 60,
-//         seconds = Math.floor(time - minutes * 60),
-
-//     return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
-// };
-
 const secondsToHms = timeInSeconds => {
     timeInSeconds = Number(timeInSeconds);
     var h = Math.floor(timeInSeconds / 3600);
