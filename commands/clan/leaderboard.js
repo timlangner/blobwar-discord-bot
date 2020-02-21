9605470fdded21a61caa81914fa8528efc765b7b
Tim <tim.langner@tobit.software>
2020-02-21 17:23:54 +0100

    :construction: Leaderboard embed

--- commands/clan/leaderboard.js
+++ commands/clan/leaderboard.js
@@ -4,7 +4,7 @@ const { prefix } = require('../../config');
 
 module.exports = {
     name: 'leaderboard',
-    description: 'Shows the fanix top 100 leaderboard',
+    description: 'Shows the fanix top 10 leaderboard',
     guildOnly: true,
     async execute(message, args) {
         if (args.length) {
@@ -20,23 +20,69 @@ module.exports = {
                     const ownerAvatar = (
                         await message.client.fetchUser('185053226641522690')
                     ).avatarURL;
-                    const leaderboardEmbed = new Discord.RichEmbed()
-                        .setColor('#0099ff')
+                    let leaderboardEmbed = new Discord.RichEmbed()
+                        .setColor('#E3B520')
                         .setTitle('Leaderboard')
                         .setURL('https://fanix.io')
-                        .addField(data[0].username, `${data[0].xp} XP`)
-                        .addField(data[1].username, `${data[1].xp} XP`)
-                        .addField(data[2].username, `${data[2].xp} XP`)
-                        .addField(data[3].username, `${data[3].xp} XP`)
-                        .addField(data[4].username, `${data[4].xp} XP`)
+                        .setDescription(
+                            'Here is the top 10 ranking leaderboard of fanix.',
+                        )
+                        .setThumbnail('https://i.imgur.com/davX7Gc.png')
                         .setFooter('Made with ♥ by Pake#0001', ownerAvatar);
 
+                    for (let i = 0; i < data.length; i++) {
+                        let userRank;
+
+                        switch (parseInt(data[i].rank)) {
+                            case 10:
+                                userRank = 'Rookie I';
+                                break;
+                            case 20:
+                                userRank = 'Rookie II';
+                                break;
+                            case 30:
+                                userRank = 'Rookie III';
+                                break;
+                            case 40:
+                                userRank = 'Hero I';
+                                break;
+                            case 50:
+                                userRank = 'Hero II';
+                                break;
+                            case 60:
+                                userRank = 'Hero III';
+                                break;
+                            case 70:
+                                userRank = 'Master I';
+                                break;
+                            case 80:
+                                userRank = 'Master II';
+                                break;
+                            case 90:
+                                userRank = 'Master III';
+                                break;
+                            case 100:
+                                userRank = 'Legend';
+                                break;
+                            default:
+                                userRank = 'undefined';
+                                break;
+                        }
+
+                        if (i <= 4) {
+                            if (i >= 1) {
+                                leaderboardEmbed.addBlankField();
+                            }
+                            leaderboardEmbed.addField(
+                                `${i + 1}. ${data[i].username}`,
+                                `${data[i].xp} XP`,
+                                true,
+                            );
+                            leaderboardEmbed.addField(`Rank: `, userRank, true);
+                        }
+                    }
+
                     message.channel.send(leaderboardEmbed);
-                })
-                .catch(() => {
-                    return message.channel.send(
-                        'Oops! Looks like something went wrong with your request. Please try it again later.',
-                    );
                 });
         }
     },
