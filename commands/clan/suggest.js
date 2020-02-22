c7e9f3fdbf7f1fe65663fb519e9e6abce88c404d
Tim Langner <tim.langner0@gmail.com>
2020-02-22 17:43:32 +0100

    :sparkles: Add suggest command

--- commands/clan/suggest.js
+++ commands/clan/suggest.js
@@ -6,33 +6,63 @@ module.exports = {
     guildOnly: true,
     async execute(message, args) {
         let title = '';
-        message.author.send('Write a title for your suggestion.').then(() => {
-            message.channel
-                .awaitMessages({
-                    maxMatches: 1,
-                    time: 30000,
-                    errors: ['time'],
-                })
-                .then(collected => {
-                    title = collected.content
-                        .join(',')
-                        .replace(/,/g, ' ')
-                        .split();
-                    console.log(title);
-                })
-                .catch(collected => {
-                    message.channel.send('Too late');
-                });
-        });
+        let description = '';
+        let filter = m => m.author.id === message.author.id;
+        let dmChannel = await message.author.createDM();
+        let isTitleMissed = false;
+        let isDescriptionMissed = false;
 
-        const userAvatar = (
-            await message.client.fetchUser(message.member.user.id)
-        ).avatarURL;
-        const suggestEmbed = new Discord.RichEmbed()
-            .setColor('#0EE34A')
-            .setTitle('New Suggestion!')
-            .setDescription(finalSuggestion)
-            .setThumbnail('https://i.imgur.com/davX7Gc.png')
-            .setFooter(`Suggested by ${message.member.user.tag}`, userAvatar);
+        await message.author.send('Enter a title for your suggestion');
+        try {
+            let msg = await message.author.dmChannel.awaitMessages(filter, {
+                maxMatches: 1,
+                time: 30000,
+                errors: ['time'],
+            });
+            title = msg.first().content;
+        } catch {
+            isTitleMissed = true;
+            message.author.send('You did not enter a title on time.');
+        }
+
+        if (!isTitleMissed) {
+            await message.author.send(
+                'Enter a description for your suggestion',
+            );
+            try {
+                let msg = await message.author.dmChannel.awaitMessages(filter, {
+                    maxMatches: 1,
+                    time: 60000,
+                    errors: ['time'],
+                });
+                description = msg.first().content;
+            } catch {
+                isDescriptionMissed = true;
+                message.author.send('You did not enter a description on time.');
+            }
+        }
+
+        if (!isTitleMissed && !isDescriptionMissed) {
+            const userAvatar = (
+                await message.client.fetchUser(message.author.id)
+            ).avatarURL;
+            const suggestionEmbed = new Discord.RichEmbed()
+                .setColor('#0099ff')
+                .setTitle(title)
+                .setDescription(description)
+                .setThumbnail('https://i.imgur.com/davX7Gc.png')
+                .setFooter(`Suggested by ${message.author.tag}`, userAvatar);
+            await message.author.send(
+                `Success! We'll send this in <#632521772071256074> where people can vote on your suggestion.`,
+            );
+            await message.author.send(suggestionEmbed);
+
+            await message.guild.channels
+                .find(c => c.id === '632521772071256074')
+                .send(suggestionEmbed)
+                .then(embed => {
+                    embed.react('✅').then(() => embed.react('❌'));
+                });
+        }
     },
 };
